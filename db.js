6179f27097b42896d75345f7e22667816c071370
Vedant <vedantpandilwad@gmail.com>
2025-02-06 10:01:54 +0530

    done

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose')
-    mongoose.connect("mongodb+srv://gauthamram:1234@cluster0.qa74x.mongodb.net/")
-    .then(()=>{
-        console.log("Connected to DataBase")
-    })
-    .catch((err)=>{console.log(`Error:${err}`)})
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://pandilwadvedants70:a1apeVDF0Q1vJ0cD@cluster0.lb80m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to DataBase");
+  })
+  .catch((err) => {
+    console.log(`Error:${err}`);
+  });
+// a1apeVDF0Q1vJ0cD
